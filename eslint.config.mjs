2d064015cbf0e70f0170bfb66584407e54f1d40c
GolamAzamShakil <golamazam1010@gmail.com>
2025-03-14 05:55:06 +0600

    set up

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,42 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import prettier from 'eslint-plugin-prettier';
+import path from 'node:path';
+
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
 });
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+//const eslintConfig = [
+ // ...compat.extends("next/core-web-vitals", "next/typescript"),
+//];
 
-export default eslintConfig;
+//export default eslintConfig;
+
+export default [
+  ...compat.extends(
+    'eslint:recommended',
+    'plugin:@next/next/recommended',
+    'plugin:prettier/recommended'
+  ),
+  {
+    plugins: {
+      prettier,
+    },
+
+    rules: {
+      'prettier/prettier': ['error'],
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': 'warn',
+    },
+  },
+];
